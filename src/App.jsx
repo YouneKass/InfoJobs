c6fcbeceeca004bf23998c89547e11e2255e14bb
felipe <felipe.lleuful@hotmail.com>
2026-03-22 15:49:11 -0300

    Cambios en parametros y estilos del detalle de cada empleo

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/jobs/:id" element={<JobDetail/>} /> 
+        <Route path="/jobs/:jobId" element={<JobDetail/>} /> 
         <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
