62fc03158ded7aad03ea01a9af4f4989f46ebb78
felipe <felipe.lleuful@hotmail.com>
2026-06-09 19:47:05 -0400

    Actualizacion

--- backend/manage-files.js
+++ backend/manage-files.js
@@ -1,5 +1,11 @@
-import {readFile} from 'node:fs/promises'
+import {mkdir, readFile, writeFile} from 'node:fs/promises'
 
 const content = await readFile('./backend/archivo.txt', 'utf-8')
 
 console.log(content)
+
+const outputDir = 'backend/output/files/documents'
+await mkdir(outputDir, { recursive: true })
+const uppercaseContent = content.toUpperCase()
+await writeFile(`./${outputDir}/archivo.uppercase.txt`, uppercaseContent)
+console.log('Archivo creado con contenido en mayúsculas')
