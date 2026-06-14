096a431049bd6e3a1f277cf0356ffd7d35dc8c7b
felipe <felipe.lleuful@hotmail.com>
2026-06-13 22:47:08 -0400

    Informaciondel sistema operativo e instalacion de paquetes

--- backend/manage-files.js
+++ backend/manage-files.js
@@ -1,11 +1,17 @@
 import {mkdir, readFile, writeFile} from 'node:fs/promises'
+import { join, basename,extname } from 'node:path'
 
-const content = await readFile('./backend/archivo.txt', 'utf-8')
-
+const content = await readFile('archivo.txt', 'utf-8')
 console.log(content)
 
-const outputDir = 'backend/output/files/documents'
+const outputDir = join('output', 'files', 'documents')
 await mkdir(outputDir, { recursive: true })
+
 const uppercaseContent = content.toUpperCase()
-await writeFile(`./${outputDir}/archivo.uppercase.txt`, uppercaseContent)
+const outputFilePath = join(outputDir, 'archivo-uppercase.txt')
+
+console.log('La extensión es: ', extname(outputFilePath))
+console.log('El nombre del archivo es: ', basename(outputFilePath))
+
+await writeFile(outputFilePath, uppercaseContent)
 console.log('Archivo creado con contenido en mayúsculas')
