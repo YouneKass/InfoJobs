a1cce1a870b572adf13127f1b8d1a709451260ae
felipe <felipe.lleuful@hotmail.com>
2026-06-29 18:19:54 -0400

    Sistema de permisos en node.js

--- backend/manage-files.js
+++ backend/manage-files.js
@@ -1,17 +1,27 @@
 import {mkdir, readFile, writeFile} from 'node:fs/promises'
 import { join, basename,extname } from 'node:path'
 
-const content = await readFile('archivo.txt', 'utf-8')
-console.log(content)
+let content = ''
 
-const outputDir = join('output', 'files', 'documents')
-await mkdir(outputDir, { recursive: true })
+if (process.permission.has('fs.read', 'archivo.txt')) {
+  content = await readFile('archivo.txt', 'utf-8')
+  console.log(content) 
+}else{
+  console.log('No tienes permiso para leer el archivo.')
+}
 
-const uppercaseContent = content.toUpperCase()
-const outputFilePath = join(outputDir, 'archivo-uppercase.txt')
+if (process.permission.has('fs.write', 'output/files/documents')) {
+  const outputDir = join('output', 'files', 'documents')
+  await mkdir(outputDir, { recursive: true })
 
-console.log('La extensión es: ', extname(outputFilePath))
-console.log('El nombre del archivo es: ', basename(outputFilePath))
+  const uppercaseContent = content.toUpperCase()
+  const outputFilePath = join(outputDir, 'archivo-uppercase.txt')
 
-await writeFile(outputFilePath, uppercaseContent)
-console.log('Archivo creado con contenido en mayúsculas')
+  console.log('La extensión es: ', extname(outputFilePath))
+  console.log('El nombre del archivo es: ', basename(outputFilePath))
+
+  await writeFile(outputFilePath, uppercaseContent)
+  console.log('Archivo creado con contenido en mayúsculas')
+}else{
+  console.log('No tienes permiso para leer el archivo.')
+}
